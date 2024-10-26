0ac397e25c8e9e98adff90de5a9696d8cdfb02df
iancu_robert <iancurobert92@gmail.com>
2024-10-26 19:26:03 +0300

    Fix test

--- src/app/services/game-board.service.spec.ts
+++ src/app/services/game-board.service.spec.ts
@@ -1,15 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
+import { MockBuilder } from 'ng-mocks';
+import { BeesService } from './bees.service';
 import { GameBoardService } from './game-board.service';
 
 describe('GameBoardService', () => {
   let service: GameBoardService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async () => {
+    await MockBuilder(GameBoardService).mock(BeesService);
     service = TestBed.inject(GameBoardService);
   });
-
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
